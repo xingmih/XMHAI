d22c5cc08fa383a8ab680fe7d111e4244d0f34e0
星觅海 <1498934815@qq.com>
2025-11-01 16:59:27 +0800

    更新 profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,18 +2,18 @@ import type { ProfileConfig } from "../types/config";
 
 export const profileConfig: ProfileConfig = {
   avatar: "/assets/images/avatar.webp",
-  name: "Firefly",
-  bio: "Hello, I'm Firefly.",
+  name: "星觅海",
+  bio: "Hello, I'm 星觅海.",
   links: [
     {
       name: "Bilibli",
       icon: "fa6-brands:bilibili",
-      url: "https://space.bilibili.com/38932988",
+      url: "https://b23.tv/EscDwn5",
     },
     {
       name: "GitHub",
       icon: "fa6-brands:github",
-      url: "https://github.com/CuteLeaf",
+      url: "https://github.com/xingmih",
     },
   ],
 };
