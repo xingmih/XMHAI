e90fc2bd31d4809acf011b60dc704b84e547a66d
星觅海 <1498934815@qq.com>
2025-11-07 17:46:29 +0800

    更新 commentConfig.ts

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -10,7 +10,7 @@ export const commentConfig: CommentConfig = {
   },
   //waline评论系统配置
   waline: {
-    serverURL: "https://waline.xmhai.cn",
+    serverURL: "https://waline.xmhai.cn/.netlify/functions/comment",
     lang: "zh-CN", // 设置 Waline 评论系统语言
     login: "enable", // 评论登录模式。可选值如下：
     //   'enable'   —— 默认，允许访客匿名评论和用第三方 OAuth 登录评论，兼容性最佳。
