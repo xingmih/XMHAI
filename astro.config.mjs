675e5ec532b0114c2d1d6504926eb8bb2a25eef5
夏叶 <xiaye@msn.com>
2026-01-09 19:55:20 +0800

    refactor: remove search indexer integration and update build script

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,6 @@ import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
-import searchIndexer from "./src/integrations/searchIndex.mts";
 import rehypeEmailProtection from "./src/plugins/rehype-email-protection.mjs";
 import rehypeFigure from "./src/plugins/rehype-figure.mjs";
 
@@ -127,7 +126,6 @@ export default defineConfig({
 				return true;
 			},
 		}),
-		searchIndexer(),
 		mdx(),
 	],
 	markdown: {
